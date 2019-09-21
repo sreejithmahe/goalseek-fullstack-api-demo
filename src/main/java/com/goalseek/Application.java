9d32635d6fa334a25f4f142a5a7dd604dbb4f040
sreejithmahe <sreejith.mahe@gmail.com>
2019-09-21 15:45:47 +0530

    SSL modification

--- src/main/java/com/goalseek/Application.java
+++ src/main/java/com/goalseek/Application.java
@@ -1,9 +1,16 @@
 package com.goalseek;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author sreejith.kizhakkayil 
@@ -21,5 +28,34 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
      SpringApplication.run(Application.class, args);
     }
+    
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+	  TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+	      @Override
+	      protected void postProcessContext(Context context) {
+	        SecurityConstraint securityConstraint = new SecurityConstraint();
+	        securityConstraint.setUserConstraint("CONFIDENTIAL");
+	        SecurityCollection collection = new SecurityCollection();
+	        collection.addPattern("/*");
+	        securityConstraint.addCollection(collection);
+	        context.addConstraint(securityConstraint);
+	      }
+	    };
+	  
+	  tomcat.addAdditionalTomcatConnectors(redirectConnector());
+	  return tomcat;
+	}
 
+	private Connector redirectConnector() {
+	  Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	  connector.setScheme("http");
+	  connector.setPort(80);
+	  connector.setSecure(false);
+	  connector.setRedirectPort(443);
+	  
+	  return connector;
+	}
 }
+
+
